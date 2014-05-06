2b3d77bb173cdd8985cde5e9d890112679d2937f
Yamishi Yuuki Nekozawa <yamishi.atarashianime@gmail.com>
2014-05-06 01:10:57 -0500

    add dynamic table controller

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+var provider = "http://mizu.yamishi.tk"
 
+$("#rastrear").click(function(){
+	$.ajax({
+		url: provider +"/" + $("#numero").val(),
+		dataType: "json",
+		type: "get",
+		success: function (data) {
+			count = data.data.length;
+			i = 0;
+			while(count > i) {
+				$("tbody").append('<tr id="row'+i+'"></tr>');
+				$("#row"+i).html('<td id="col'+i+0+'">'+data.data[i].fecha+'</td>'+
+					'<td id="col'+i+1+'">'+data.data[i].origen+'</td>'+
+					'<td id="col'+i+2+'">'+data.data[i].accion+'</td>'+
+					'<td id="col'+i+3+'">'+data.data[i].destino+'</td>');
+				i++;
+			}
+		}
+	});
+});
